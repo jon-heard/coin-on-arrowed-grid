4e45878c7c1fadd5bd9dcfe0d8160ef645b8668c
Jon Heard <nethermonkey@gmail.com>
2017-06-11 01:21:31 -0400

    Got clicking and neighbors working, texture rename: 'chip'->'coin'

--- src/index.ts
+++ src/index.ts
@@ -31,7 +31,7 @@ function initialize() {
         .add('arrow_right', 'images/arrow_right.png')
         .add('arrow_down', 'images/arrow_down.png')
         .add('arrow_left', 'images/arrow_left.png')
-        .add('chip',    'images/chip.png')
+        .add('coin',    'images/coin.png')
         .load(function (loader:PIXI.loaders.Loader, newResources:any) {
             resources = newResources;
             arrowTextures[0] = resources.arrow_up.texture;
@@ -45,11 +45,13 @@ function initialize() {
 }
 
 function generateUi() {
+    // Setup style
     fontStyle = new PIXI.TextStyle({
         fontFamily: 'Arial',
         fontSize: 20,
         fill: 'white',
     });
+    // Setup help text
     helpText = new PIXI.Text("Click on a tile to drop a coin.\nSee how long you can get it to stay before falling off the board.", fontStyle);
     helpText.x = HELP_OFFSET_X;
     helpText.y = HELP_OFFSET_Y;
@@ -58,19 +60,35 @@ function generateUi() {
 }
 
 function generateBoard() {
+    // Setup sprites
     for (var y = 0; y < boardSize; ++y) {
         board[y] = [];
         for (var x = 0; x < boardSize; ++x) {
-            var tile = ((x + y) % 2 == 0) ? resources.tile1.texture : resources.tile2.texture;
+            var tileTexture = ((x + y) % 2 == 0) ? resources.tile1.texture : resources.tile2.texture;
             var positionX = x * (64 + BOARD_PADDING_X) + BOARD_OFFSET_X;
             var positionY = y * (64 + BOARD_PADDING_Y) + BOARD_OFFSET_Y;
-            board[y][x] = new PIXI.Sprite(tile);
-            board[y][x].position.x = positionX;
-            board[y][x].position.y = positionY;
-            board[y][x].arrow = new PIXI.Sprite(arrowTextures[2]);
-            board[y][x].arrow.type = 2;
-            board[y][x].addChild(board[y][x].arrow);
-            stage.addChild(board[y][x]);
+            board[y][x] = new PIXI.Sprite(tileTexture);
+            var tile = board[y][x];
+            tile.position.x = positionX;
+            tile.position.y = positionY;
+            tile.arrow = new PIXI.Sprite(arrowTextures[2]);
+            tile.arrowType = 2;
+            tile.addChild(tile.arrow);
+            tile.interactive = true;
+            tile.buttonMode = true;
+            tile.on('mouseup', boardClick);
+            stage.addChild(tile);
+        }
+    }
+    // Setup neighbor links
+    for (var y = 0; y < boardSize; ++y) {
+        for (var x = 0; x < boardSize; ++x) {
+            var tile = board[y][x];
+            tile.neighbor = [];
+            if (y > 0)           { tile.neighbor[0] = board[y-1][x]; }
+            if (x < boardSize-1) { tile.neighbor[1] = board[y][x+1]; }
+            if (y < boardSize-1) { tile.neighbor[2] = board[y+1][x]; }
+            if (x > 0)           { tile.neighbor[3] = board[y][x-1]; }
         }
     }
     randomizeBoard();
@@ -81,8 +99,18 @@ function randomizeBoard() {
         for (var x = 0; x < boardSize; ++x) {
             var type = Math.floor(Math.random() * 4);
             board[y][x].arrow.texture = arrowTextures[type];
-            board[y][x].arrow.type = type;
+            board[y][x].arrowType = type;
         }
+    }
+}
+
+function boardClick(event) {
+    var tile = event.target;
+    tile = tile.neighbor[tile.arrowType];
+    if (tile == null) {
+    } else {
+        tile.coin = new PIXI.Sprite(resources.coin.texture);
+        tile.addChild(tile.coin);
     }
 }
 
