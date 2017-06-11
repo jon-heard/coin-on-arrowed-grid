99f4162b4ba50225eb6d784b4f12ce5bfce4ac3a
Jon Heard <nethermonkey@gmail.com>
2017-06-11 14:59:44 -0400

    Added coin animation

--- src/index.ts
+++ src/index.ts
@@ -13,14 +13,16 @@ import PIXI = require('pixi.js');
     const HELP_OFFSET_Y = 10;
     const BOARD_OFFSET_X = 10;
     const BOARD_OFFSET_Y = 70;
-    const BOARD_PADDING_X = 4;
-    const BOARD_PADDING_Y = 4;
-    const FRAME_SPEED = 200;
+    const BOARD_PADDING_X = 2;
+    const BOARD_PADDING_Y = 2;
+    const FRAME_SPEED = 100;
+    const coinAnimationFrames = [[0,3,4,0],[0,1,2,0],[0,4,3,0],[0,2,1,0]];
     // game resources
     let resources:any;
     var fontStyle;
     var arrowTextures;
     var explosionTextures;
+    var coinTextures;
     var helpText;
     // game state
     var boardSize = 10;
@@ -48,13 +50,17 @@ function initialize() {
         .add('explosion_4', 'images/explosion_4.png')
         .add('explosion_5', 'images/explosion_5.png')
         .add('coin',    'images/coin.png')
+        .add('coin_horizontal1',    'images/coin_horizontal1.png')
+        .add('coin_horizontal2',    'images/coin_horizontal2.png')
+        .add('coin_vertical1',    'images/coin_vertical1.png')
+        .add('coin_vertical2',    'images/coin_vertical2.png')
         .load(function (loader:PIXI.loaders.Loader, newResources:any) {
             resources = newResources;
             setupTextureResources();
             setupUi();
             setupBoard();
             setupCoinAndExplosion();
-            runAnimation(0);
+            runMainLoop(0);
         });
 }
 
@@ -70,6 +76,12 @@ function setupTextureResources() {
     explosionTextures[2] = resources.explosion_3.texture;
     explosionTextures[3] = resources.explosion_4.texture;
     explosionTextures[4] = resources.explosion_5.texture;
+    coinTextures = [];
+    coinTextures[0] = resources.coin.texture;
+    coinTextures[1] = resources.coin_horizontal1.texture;
+    coinTextures[2] = resources.coin_horizontal2.texture;
+    coinTextures[3] = resources.coin_vertical1.texture;
+    coinTextures[4] = resources.coin_vertical2.texture;
 }
 
 function setupUi() {
@@ -126,6 +138,7 @@ function setupBoard() {
 
 function setupCoinAndExplosion() {
     coin = new PIXI.Sprite(resources.coin.texture);
+    coin.frame = -1;
     coin.visible = false;
     stage.addChild(coin);
     explosion = new PIXI.Sprite(explosionTextures[0]);
@@ -150,19 +163,27 @@ function onBoardClick(event) {
     explosion.visible = false;
     // place the coin
     coin.tile = tile; 
-    coin.position = tile.position; 
+    coin.position = tile.position;
+    coin.direction = tile.arrowType;
     coin.visible = true;
 }
 
 function runFrameLogic() {
-    if (coin.visible && !explosion.visible) {
+    // animate coin
+    if (coin.visible) {
+      coin.frame += 1;
+      if (coin.frame > 3) { coin.frame = 0; }
+      coin.texture = coinTextures[coinAnimationFrames[coin.direction][coin.frame]];
+    }
+    // handle coin movement
+    if (coin.visible && coin.frame == 2 && !explosion.visible) {
         var tile = coin.tile;
-        // add trail
+        // add to coin trail
         var newCoinTrail = new PIXI.Sprite(resources.coin.texture);
         coinTrail.push(newCoinTrail);
         newCoinTrail.position = coin.position;
         newCoinTrail["tile"] = coin.tile;
-        newCoinTrail.alpha = .5;
+        newCoinTrail.alpha = .35;
         stage.addChild(newCoinTrail);
         // Get new position
         var newTile = tile.neighbor[tile.arrowType];
@@ -184,13 +205,16 @@ function runFrameLogic() {
             }
             if (!explosion.visible) {
                 coin.visible = false;
+                coin.frame = -1;
             }
         // move coin
         } else {
             coin.tile = newTile;
             coin.position = newTile.position;
+            coin.direction = newTile.arrowType;
         }
     }
+    // animate explosion
     if (explosion.visible) {
         ++explosion.frame;
         // special frames
@@ -198,6 +222,7 @@ function runFrameLogic() {
           explosion.position = coin.position;
         } else if (explosion.frame == 1) {
           coin.visible = false;
+          coin.frame = -1;
         }
         // Draw or remove
         if (explosion.frame == 5) {
@@ -209,7 +234,7 @@ function runFrameLogic() {
     }
 }
 
-function runAnimation(timeStamp) {
+function runMainLoop(timeStamp) {
     // call "runFrameLogic()" at FRAME_SPEED intervals
     if (timeStamp-frameTime > FRAME_SPEED) {
         frameTime = timeStamp;
@@ -217,7 +242,7 @@ function runAnimation(timeStamp) {
     }
     // Render and loop
     renderer.render(stage);
-    requestAnimationFrame(runAnimation);
+    requestAnimationFrame(runMainLoop);
 }
 
 initialize();
