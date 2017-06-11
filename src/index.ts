af66876d2253eb0c7e697219922d9b101e648331
Jon Heard <nethermonkey@gmail.com>
2017-06-11 11:46:15 -0400

    Added coin explosion on infinite loop: distinguishes from off edge

--- src/index.ts
+++ src/index.ts
@@ -15,16 +15,18 @@ import PIXI = require('pixi.js');
     const BOARD_OFFSET_Y = 70;
     const BOARD_PADDING_X = 4;
     const BOARD_PADDING_Y = 4;
-    const FRAME_SPEED = 100;
+    const FRAME_SPEED = 200;
     // game resources
     let resources:any;
     var fontStyle;
     var arrowTextures;
+    var explosionTextures;
     var helpText;
     // game state
     var boardSize = 10;
     var board = [];
     var coin;
+    var explosion;
     var coinTrail = [];
     var frameTime = 0;
 
@@ -40,23 +42,34 @@ function initialize() {
         .add('arrow_right', 'images/arrow_right.png')
         .add('arrow_down', 'images/arrow_down.png')
         .add('arrow_left', 'images/arrow_left.png')
+        .add('explosion_1', 'images/explosion_1.png')
+        .add('explosion_2', 'images/explosion_2.png')
+        .add('explosion_3', 'images/explosion_3.png')
+        .add('explosion_4', 'images/explosion_4.png')
+        .add('explosion_5', 'images/explosion_5.png')
         .add('coin',    'images/coin.png')
         .load(function (loader:PIXI.loaders.Loader, newResources:any) {
             resources = newResources;
-            setupArrows();
+            setupTextureResources();
             setupUi();
             setupBoard();
-            setupCoin();
+            setupCoinAndExplosion();
             runAnimation(0);
         });
 }
 
-function setupArrows() {
+function setupTextureResources() {
     arrowTextures = [];
     arrowTextures[0] = resources.arrow_up.texture;
     arrowTextures[1] = resources.arrow_right.texture;
     arrowTextures[2] = resources.arrow_down.texture;
     arrowTextures[3] = resources.arrow_left.texture;
+    explosionTextures = [];
+    explosionTextures[0] = resources.explosion_1.texture;
+    explosionTextures[1] = resources.explosion_2.texture;
+    explosionTextures[2] = resources.explosion_3.texture;
+    explosionTextures[3] = resources.explosion_4.texture;
+    explosionTextures[4] = resources.explosion_5.texture;
 }
 
 function setupUi() {
@@ -67,7 +80,7 @@ function setupUi() {
         fill: 'white',
     });
     // Setup help text
-    helpText = new PIXI.Text("Click on a tile to drop a coin.\nSee how long you can get it to stay before falling off the board.", fontStyle);
+    helpText = new PIXI.Text("Click on a tile to drop a coin.\nSee quickly you can find the paths off of the board.", fontStyle);
     helpText.x = HELP_OFFSET_X;
     helpText.y = HELP_OFFSET_Y;
     stage.addChild(helpText);
@@ -111,10 +124,14 @@ function setupBoard() {
     randomizeBoard();
 }
 
-function setupCoin() {
+function setupCoinAndExplosion() {
     coin = new PIXI.Sprite(resources.coin.texture);
     coin.visible = false;
     stage.addChild(coin);
+    explosion = new PIXI.Sprite(explosionTextures[0]);
+    explosion.frame = -1;
+    explosion.visible = false;
+    stage.addChild(explosion);
 }
 
 function randomizeBoard() {
@@ -129,14 +146,16 @@ function randomizeBoard() {
 
 function onBoardClick(event) {
     var tile = event.target;
-    // On click, place the coin
+    // remove the explosion (if still visible)
+    explosion.visible = false;
+    // place the coin
     coin.tile = tile; 
     coin.position = tile.position; 
     coin.visible = true;
 }
 
 function runFrameLogic() {
-    if (coin.visible) {
+    if (coin.visible && !explosion.visible) {
         var tile = coin.tile;
         // add trail
         var newCoinTrail = new PIXI.Sprite(resources.coin.texture);
@@ -145,28 +164,47 @@ function runFrameLogic() {
         newCoinTrail["tile"] = coin.tile;
         newCoinTrail.alpha = .5;
         stage.addChild(newCoinTrail);
-        // move coin
-        tile = tile.neighbor[tile.arrowType];
-        if (tile) {
-            coin.tile = tile;
-            coin.position = tile.position;
-        }
+        // Get new position
+        var newTile = tile.neighbor[tile.arrowType];
         // Check for trail loop
-        var isOnCoinTrail = false;
-        for (var i = 0; i < coinTrail.length; ++i) {
-            if (coinTrail[i].tile == coin.tile) {
-                isOnCoinTrail = true;
-                break;
-            }
+        if (newTile) {
+          for (var i = 0; i < coinTrail.length; ++i) {
+              if (coinTrail[i].tile == newTile) {
+                  explosion.visible = true;
+                  break;
+              }
+          }
         }
-        // Remove coin (if applicable
-        if (!tile || isOnCoinTrail) {
-            coin.visible = false;
+        // Remove coin (if applicable)
+        if (!newTile || explosion.visible) {
             while (coinTrail.length > 0) {
                 var coinTrailItem = coinTrail.pop();
                 stage.removeChild(coinTrailItem);
                 coinTrailItem.destroy();
             }
+            if (!explosion.visible) {
+                coin.visible = false;
+            }
+        // move coin
+        } else {
+            coin.tile = newTile;
+            coin.position = newTile.position;
+        }
+    }
+    if (explosion.visible) {
+        ++explosion.frame;
+        // special frames
+        if (explosion.frame == 0) {
+          explosion.position = coin.position;
+        } else if (explosion.frame == 1) {
+          coin.visible = false;
+        }
+        // Draw or remove
+        if (explosion.frame == 5) {
+            explosion.visible = false;
+            explosion.frame = -1;
+        } else {
+            explosion.texture = explosionTextures[explosion.frame];
         }
     }
 }
